bcd59741fbfaf2d59bc37a35aa652c06fa5a1ca7
Benjámin Ferge <benjamin.ferge@protonmail.com>
2021-08-03 16:00:03 +0200

    Make react-player responsive

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import ReactPlayer from "react-player";
 
 export default function Video() {
-    return <ReactPlayer
-        url={[{ src: "https://my-bucket.eu-central-1.linodeobjects.com/konradvofely_hu/kisvofely.mp4", type: "video/mp4" }]}
-        controls={true}
-        light={"https://my-bucket.eu-central-1.linodeobjects.com/konradvofely_hu/kisvofely_preview.jpg"}
-        config={{
-            file: {
-                attributes: {
-                    controlsList: "nodownload"
-                }
-            }
-        }}
-    />;
+    return (
+        <div className="player-wrapper">
+            <ReactPlayer
+                className="react-player"
+                url={[{ src: "https://my-bucket.eu-central-1.linodeobjects.com/konradvofely_hu/kisvofely.mp4", type: "video/mp4" }]}
+                controls
+                playing
+                width="100%"
+                height="100%"
+                light={"https://my-bucket.eu-central-1.linodeobjects.com/konradvofely_hu/kisvofely_preview.jpg"}
+                config={{
+                    file: {
+                        attributes: {
+                            controlsList: "nodownload"
+                        }
+                    }
+                }}
+            />
+        </div>
+    );
 }
